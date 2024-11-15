d301ca8397c730df96543a4943228b902eca9c82
Yarin Kovalski <yarin.kovalski@post.runi.ac.il>
2024-11-15 21:13:48 +0200

    exe 4 third try

--- CalcPi.java
+++ CalcPi.java
@@ -2,28 +2,21 @@
 public class CalcPi {
 	public static void main(String [] args) 
 	{ 
-		int n = Integer.parseInt("" + args[0]);
+		int numTerms = Integer.parseInt("" + args[0]);
 		double dividor = 3;
-		double sum = 1;
+		double sum = 0;
 		boolean flag = false;
-		for (int i=0; i<=n; i++)
+        for (int i = 0; i < numTerms; i++) 
 		{
-			if (flag == false)
-				{
-					sum = sum - (1.0/dividor);
-					flag = true;
-				}
-
-			else
-				{
-					sum = sum + (1.0/dividor);
-					flag = false;
-				}
-			dividor = dividor +2;
-
-		}
+            double term = 1.0 / (2 * i + 1);
+            if (i % 2 == 0) {
+                sum = sum + term; 
+            } else {
+                sum = sum - term; 
+            }
+        }
 		
 		System.out.println("pi according to Java: 3.141592653589793");
-		System.out.println("pi, approximated: " + (sum)*4.0);
+		System.out.println("pi, approximated: " +(4 * sum));
 	}
 }
